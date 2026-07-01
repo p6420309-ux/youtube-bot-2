d24b0e37e86e7438b6f34c55cf1f38cb86fb3be2
p6420309-ux <p6420309@gmail.com>
2026-07-01 12:15:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,25 @@
 import os
 
-
 class Config:
+    # यहाँ अपना Telegram Bot Token डालें (जैसे: "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11")
+    BOT_TOKEN = "8955666230:AAEWGCBuDv9y94OaV0RUMqNZjArN2LO1Ljc"
 
-    # पुराने फाइल से डाला गया BOT_TOKEN
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8955666230:AAEWGCBuDv9y94OaV0RUMqNZjArN2LO1Ljc")
+    SESSION_NAME = "utube_bot"
 
-    # पुराने फाइल से डाला गया SESSION_NAME
-    SESSION_NAME = os.environ.get("SESSION_NAME", "utube_bot")
+    # API_ID नंबर होता है, इसलिए इसमें "" नहीं लगेगा
+    API_ID = 33001782
 
-    # पुराने फाइल से डाला गया API_ID
-    API_ID = int(os.environ.get("API_ID", "33001782"))
+    # API_HASH टेक्स्ट है, इसलिए यह "" के अंदर रहेगा
+    API_HASH = "c2e205b6e5d630380bb83d4c69d5b977"
 
-    # पुराने फाइल से डाला गया API_HASH
-    API_HASH = os.environ.get("API_HASH", "c2e205b6e5d630380bb83d4c69d5b977")
+    CLIENT_ID = "251281373720-ug7q7906b479msc6p5h4ffud69u97f2f.apps.googleusercontent.com"
 
-    # पुराने फाइल से डाला गया CLIENT_ID
-    CLIENT_ID = os.environ.get("CLIENT_ID", "251281373720-ug7q7906b479msc6p5h4ffud69u97f2f.apps.googleusercontent.com")
+    CLIENT_SECRET = "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"
 
-    # पुराने फाइल से डाला गया CLIENT_SECRET
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "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")
+    # BOT_OWNER नंबर है, इसलिए "" नहीं लगेगा
+    BOT_OWNER = 8783087041
 
-    # पुराने फाइल से डाला गया BOT_OWNER
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "8783087041"))
-
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = ""
 
     AUTH_USERS = [BOT_OWNER, 374321319] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
@@ -32,26 +27,12 @@ class Config:
         else []
     )
 
-    VIDEO_DESCRIPTION = (
-        os.environ.get("VIDEO_DESCRIPTION", "").replace("<", "").replace(">", "")
-    )
+    VIDEO_DESCRIPTION = ""
+    VIDEO_CATEGORY = 0
+    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_SUFFIX = ""
+    DEBUG = False
 
-    VIDEO_CATEGORY = (
-        int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
-    )
-
-    VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
-
-    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "")
-
-    DEBUG = bool(os.environ.get("DEBUG", False))
-
-    # पुराने फाइल से डाला गया UPLOAD_MODE
-    UPLOAD_MODE = os.environ.get("UPLOAD_MODE", "private") or False
-    if UPLOAD_MODE:
-        if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
-            UPLOAD_MODE = UPLOAD_MODE.lower()
-        else:
-            UPLOAD_MODE = False
+    UPLOAD_MODE = "private" 
 
     CRED_FILE = "auth_token.txt"
